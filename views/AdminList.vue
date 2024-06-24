f25f9bcf2dff913c5497233f84b6b936be6880bb
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2024-06-24 07:09:59 +0300

    add: filtered client list

--- views/AdminList.vue
+++ views/AdminList.vue
@@ -1,9 +1,11 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 import { useRouter } from 'vue-router';
 import { getAllClients } from "../api/admin.js";
 
 const clients = ref([]);
+const filteredClients = ref([]);
+const filter = ref('all');
 
 const router = useRouter();
 
@@ -11,20 +13,39 @@ const goToClientDetail = (id) => {
   router.push({ name: 'ClientDetail', params: { id } });
 };
 
+const applyFilter = () => {
+  if (filter.value === 'all') {
+    filteredClients.value = clients.value;
+  } else if (filter.value === 'no_test') {
+    filteredClients.value = clients.value.filter(client => client.status === 'Нет теста');
+  } else if (filter.value === 'with_test') {
+    filteredClients.value = clients.value.filter(client => client.status !== 'Нет теста');
+  }
+};
+
 onMounted(() => {
   getAllClients()
       .then((res) => {
+        console.log(res.data.rows);
         clients.value = res.data.rows;
+        applyFilter();
       })
       .catch((e) => console.error(e));
-})
+});
+
+watch(filter, applyFilter);
 </script>
 
 <template>
   <h2>Список клиентов</h2>
+  <div class="button-wrapper">
+    <Button label="Все пользователи" severity="help" @click="filter = 'all'"/>
+    <Button label="Новые пользователи" @click="filter = 'no_test'"/>
+    <Button label="Прошли тестирование" severity="warn" @click="filter = 'with_test'"/>
+  </div>
   <div class="client-list">
     <ul>
-      <li v-for="client in clients" :key="client.id" @click="goToClientDetail(client.id)">
+      <li v-for="client in filteredClients" :key="client.id" @click="goToClientDetail(client.id)">
         {{ client.firstName }} {{ client.lastName }}.
         <span>Статус: {{ client.status }}</span>
       </li>
@@ -33,6 +54,10 @@ onMounted(() => {
 </template>
 
 <style scoped>
+.button-wrapper {
+  @apply mt-2 flex justify-center gap-4;
+}
+
 .client-list {
   max-width: 600px;
   margin: 0 auto;
